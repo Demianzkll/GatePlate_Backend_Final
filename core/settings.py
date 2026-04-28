b26737d57a7fe4e529248a37d1885afa250dc567
Demian <demanzakaluzhnij@gmail.com>
2026-04-28 22:24:23 +0300

    Fix SQL syntax error 1064

--- core/settings.py
+++ core/settings.py
@@ -184,8 +184,7 @@ DATABASES = {
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "3306"),
         "OPTIONS": {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', names='utf8mb4'",
-            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', names 'utf8mb4'",
             'ssl': {
                 'ca': os.path.join(BASE_DIR, 'ca.pem'),
             },
